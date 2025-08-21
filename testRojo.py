3f9a39551945ee06b5e11192b1b60cd0c0debdcc
Jared <jaredfernandezm2006@gmail.com>
2025-08-21 04:03:18 -0500

    Añadir test que falla (Rojo).
    Se muestra el test que falla debido a que no conoce la función "mayor".

---
+++ testRojo.py
@@ -0,0 +1,10 @@
+try:
+    def probar_tests_mayor():
+        assert mayor([3, 9, 2]) == 9
+        assert mayor([]) is None
+
+    probar_tests_mayor()
+    print("Todos los tests PASARON (verde) ✅")
+except Exception as e:
+    import traceback
+    traceback.print_exc()
